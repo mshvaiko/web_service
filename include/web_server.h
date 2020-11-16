95d2aab6ac1a819f6e6609eea21a33b3a9f44886
mshvaiko <shvayko.maxim@gmail.com>
2020-11-16 13:03:30 +0200

    Draft version of browser response
    Signed-off-by: mshvaiko <shvayko.maxim@gmail.com>

---
+++ include/web_server.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+namespace server
+{
+    
+class web_server
+{
+private:
+    std::string address;
+    int port;
+    int sock_fd;
+public:
+    web_server(const std::string& address_, int port_);
+    ~web_server() = default;
+
+    bool init();
+
+    void run();
+};
+
+} // namespace server
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "web_server.h"
+
 int main() {
 	std::cout << "hello" << std::endl;
+
+	server::web_server srv("127.0.0.1", 8080);
+	auto res = srv.init();
+
+	if (res) {
+		srv.run();
+	} else
+	{
+		std::cout << "Can't run\n";
+	}
+	
 	return 0;
 }
